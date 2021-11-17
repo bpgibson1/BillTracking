3df6113dec8f8e3211d1d64b9808b08380e6eec5
Bryner <bpgibson1@dmacc.edu>
2021-11-17 01:54:02 +0000

    added variables to bill class

--- src/main/java/dmacc/beans/Bill.java
+++ src/main/java/dmacc/beans/Bill.java
@@ -15,5 +15,43 @@ public class Bill {
 	@Id
 	@GeneratedValue
 	private long id;
+	private int total;
+	private float tax;
+	private String desc;
+	private String activity;
+	//private Customer customer;
+	
+	//TODO: constructors
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getActivity() {
+		return activity;
+	}
+	public void setActivity(String activity) {
+		this.activity = activity;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public float getTax() {
+		return tax;
+	}
+	public void setTax(float tax) {
+		this.tax = tax;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
 	
 }
