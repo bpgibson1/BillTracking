ad0223bfdecf32b87b9ec1544ac05a049a733a07
Arda <ardarugji1@gmail.com>
2021-12-01 16:55:04 +0100

    Fixed bugs
    Changes in bill.java file

--- src/main/java/dmacc/beans/Bill.java
+++ src/main/java/dmacc/beans/Bill.java
@@ -13,101 +13,109 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Bill {
-	
+
 	@Id
 	@GeneratedValue
 	private long id;
 	private int total;
 	private float tax;
-	private String desc;
+	private String description;
 	private String activity;
 	private int paid = 0;
-	//private Customer customer; customer will inherit from person
-	//date variable *** look at input page for further guidance on variable name ***
-	
+	// private Customer customer; customer will inherit from person
+	// date variable *** look at input page for further guidance on variable name
+	// ***
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Employee employee;
 	private long empId;
-	
-	//TODO: constructors
-	public Bill(long id, int total, float tax, String desc, String activity) {
+
+	// TODO: constructors
+	public Bill(long id, int total, float tax, String description, String activity) {
 		super();
 		this.id = id;
 		this.total = total;
 		this.tax = tax;
-		this.desc = desc;
+		this.description = description;
 		this.activity = activity;
 	}
-	
-	public Bill(int total, float tax, String desc, String activity) {
+
+	public Bill(int total, float tax, String description, String activity) {
 		super();
 		this.total = total;
 		this.tax = tax;
-		this.desc = desc;
+		this.description = description;
 		this.activity = activity;
 	}
-	
+
 	public Bill(int total, float tax) {
 		super();
 		this.total = total;
 		this.tax = tax;
 		this.paid = 0;
 	}
-	
+
 	public Bill(int total) {
 		this.total = total;
 	}
-	
+
 	public Bill() {
 		super();
 	}
-	
+
 	public Employee getEmployee() {
 		return employee;
 	}
+
 	public void setEmployee(Employee employee) {
 		this.employee = employee;
 		this.setEmpId(employee.getId());
 	}
-	
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
-	
+
 	public String getActivity() {
 		return activity;
 	}
+
 	public void setActivity(String activity) {
 		this.activity = activity;
 	}
-	
+
 	public int getTotal() {
 		return total;
 	}
+
 	public void setTotal(int total) {
 		this.total = total;
 	}
-	
+
 	public float getTax() {
 		return tax;
 	}
+
 	public void setTax(float tax) {
 		this.tax = tax;
 	}
-	
-	public String getDesc() {
-		return desc;
+
+	public String getDescription() {
+		return description;
 	}
-	public void setDesc(String desc) {
-		this.desc = desc;
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public long getEmpId() {
 		return empId;
 	}
+
 	public void setEmpId(long empId) {
 		this.empId = empId;
 	}
@@ -119,5 +127,5 @@ public class Bill {
 	public void setPaid(int paid) {
 		this.paid = paid;
 	}
-	
+
 }
