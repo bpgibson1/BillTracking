302e686f901935498bb51f194a736449967d4442
ddgibson <ddgibson@dmacc.edu>
2021-12-03 02:37:07 +0000

    added delete and fixed a problem

--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import dmacc.beans.Bill;
@@ -54,6 +55,26 @@ public class WebController {
 	@PostMapping("/inputBill")
 	public String addNewBill(@ModelAttribute Bill b, Model model) {
 		repo.save(b);
+		return viewAllBills(model);
+	}
+	
+	@GetMapping("/edit/{id}") 
+	 public String updateBill(@PathVariable("id") long id, Model model) { 
+	  Bill b = repo.findById(id).orElse(null); 
+	  model.addAttribute("newBill", b); 
+	  return "input"; 
+	 }
+	
+	@PostMapping("/update/{id}") 
+	 public String reviseBill(Bill b, Model model) { 
+	  repo.save(b); 
+	  return viewAllBills(model); 
+	 }
+	
+	@GetMapping("/delete/{id}") 
+	 public String deleteUser(@PathVariable("id") long id, Model model) { 
+		Bill b = repo.findById(id).orElse(null); 
+		repo.delete(b); 
 		return viewAllBills(model);
 	}
 	
